23fec81d563cf89e2aa17b6acf5b10fb470d79a7
alexmclean <alexmclean206@berkeley.edu>
2015-10-28 17:23:34 -0700

    attached server to index.html, working on creating cron job

--- web/http-helpers.js
+++ web/http-helpers.js
@@ -14,6 +14,15 @@ exports.serveAssets = function(res, asset, callback) {
   // Write some code here that helps serve up your static files!
   // (Static files are things like html (yours or archived from others...),
   // css, or anything that doesn't change often.)
+  res.writeHead(200, headers);
+  res.write(asset);
+  res.end();
+};
+
+exports.handleResponse = function(response, statusCode, data){
+  response.writeHead(statusCode, headers);
+  response.write(data);
+  response.end();
 };
 
   // Write some code here that helps serve up your static files!
--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,36 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
+var url = require('url');
+var http = require('./http-helpers');
+var fs = require('fs');
 // require more modules/folders here!
 
 exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+  if(req.method === "GET"){
+    var parsedURL = url.parse(req.url);
+    console.log(parsedURL);
+    //res.writeHead()
+    fs.readFile('./public/index.html', function (err, data) {
+      if (err) throw err;
+      console.log(data);
+      http.serveAssets(res, data);
+    });
+
+  } else if(req.method === "POST"){
+    console.log(archive.paths.list);
+    var writer = fs.createWriteStream(archive.paths.list, {flags:'a'});
+    var data = '';
+    req.on('data', function(chunk) {
+      data += chunk;
+    });
+
+    req.on('end', function(){
+      console.log(data);
+      writer.write(data);
+      http.handleResponse(res, 200, data);
+    });
+
+  } else {
+    res.end(archive.paths.list);
+  }
 };
